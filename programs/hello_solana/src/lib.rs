e3ad238e8a6d29e86f6c16f04557fc8dc00efe01
HelloX3030 <120511705+HelloX3030@users.noreply.github.com>
2025-08-06 13:43:25 +0000

    new greeting system self writen

--- programs/hello_solana/src/lib.rs
+++ programs/hello_solana/src/lib.rs
@@ -1,45 +1,3 @@
-use anchor_lang::prelude::*;
-
-declare_id!("13xv7grzU9AXt7S4dH51ZuyKwmgduTbtM6WBKk3B4FBq");
-
-#[program]
-pub mod hello_solana {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>, message: String) -> Result<()> {
-        let base_account = &mut ctx.accounts.base_account;
-        base_account.greeting = message;
-        Ok(())
-    }
-
-    pub fn update(ctx: Context<Update>, new_message: String) -> Result<()> {
-        let base_account = &mut ctx.accounts.base_account;
-        base_account.greeting = new_message;
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 64)]
-    pub base_account: Account<'info, GreetingAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub base_account: Account<'info, GreetingAccount>,
-}
-
-#[account]
-pub struct GreetingAccount {
-    pub greeting: String,
-}
-
-// ---------------------------------------------------------------------------------------------- //
 // use anchor_lang::prelude::*;
 
 // declare_id!("13xv7grzU9AXt7S4dH51ZuyKwmgduTbtM6WBKk3B4FBq");
@@ -48,11 +6,15 @@ pub struct GreetingAccount {
 // pub mod hello_solana {
 //     use super::*;
 
-//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+//     pub fn initialize(ctx: Context<Initialize>, message: String) -> Result<()> {
 //         let base_account = &mut ctx.accounts.base_account;
-//         base_account.owner = ctx.accounts.user.key();
-//         base_account.amount = 0;
-//         base_account.self_greets = 0;
+//         base_account.greeting = message;
+//         Ok(())
+//     }
+
+//     pub fn update(ctx: Context<Update>, new_message: String) -> Result<()> {
+//         let base_account = &mut ctx.accounts.base_account;
+//         base_account.greeting = new_message;
 //         Ok(())
 //     }
 // }
@@ -60,15 +22,73 @@ pub struct GreetingAccount {
 // #[derive(Accounts)]
 // pub struct Initialize<'info> {
 //     #[account(init, payer = user, space = 8 + 64)]
-//     pub base_account: Account<'info, GreetingUser>,
+//     pub base_account: Account<'info, GreetingAccount>,
 //     #[account(mut)]
 //     pub user: Signer<'info>,
 //     pub system_program: Program<'info, System>,
 // }
 
-// #[account]
-// pub struct GreetingUser {
-//     pub owner: Pubkey,
-//     pub amount: u64,
-//     pub self_greets: u64,
+// #[derive(Accounts)]
+// pub struct Update<'info> {
+//     #[account(mut)]
+//     pub base_account: Account<'info, GreetingAccount>,
 // }
+
+// #[account]
+// pub struct GreetingAccount {
+//     pub greeting: String,
+// }
+
+// ---------------------------------------------------------------------------------------------- //
+use anchor_lang::prelude::*;
+
+declare_id!("13xv7grzU9AXt7S4dH51ZuyKwmgduTbtM6WBKk3B4FBq");
+
+#[program]
+pub mod hello_solana {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.owner = ctx.accounts.user.key();
+        base_account.amount = 0;
+        base_account.self_greets = 0;
+        Ok(())
+    }
+
+    pub fn greet(ctx: Context<Greet>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user_key = ctx.accounts.user.key();
+
+        if user_key == base_account.owner {
+            base_account.self_greets += 1;
+        } else {
+            base_account.amount += 1;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 32 + 8 + 8)] // => Discrimnator + Pubkey + u64 + u64
+    pub base_account: Account<'info, GreetingUser>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Greet<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, GreetingUser>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct GreetingUser {
+    pub owner: Pubkey,
+    pub amount: u64,
+    pub self_greets: u64,
+}
--- tests/hello_solana.ts
+++ tests/hello_solana.ts
@@ -3,18 +3,59 @@ import { Program } from "@coral-xyz/anchor";
 import { HelloSolana } from "../target/types/hello_solana";
 import { assert } from "chai";
 
+// describe("hello_solana", () => {
+//   // Configure the client to use the local cluster.
+//   const provider = anchor.AnchorProvider.env();
+//   anchor.setProvider(provider);
+
+//   const program = anchor.workspace.HelloSolana as Program<HelloSolana>;
+
+//   let baseAccount = anchor.web3.Keypair.generate();
+
+//   it("Is initialized!", async () => {
+//     await program.methods
+//       .initialize("Hello Solana!")
+//       .accounts({
+//         baseAccount: baseAccount.publicKey,
+//         user: provider.wallet.publicKey,
+//       })
+//       .signers([baseAccount])
+//       .rpc();
+
+//     const account = await program.account.greetingAccount.fetch(
+//       baseAccount.publicKey
+//     );
+//     console.log("Stored greeting:", account.greeting);
+//     assert.equal(account.greeting, "Hello Solana!");
+//   });
+
+//   it("Can update the message", async () => {
+//     await program.methods
+//       .update("Updated greeting")
+//       .accounts({
+//         baseAccount: baseAccount.publicKey,
+//       })
+//       .rpc();
+
+//     const account = await program.account.greetingAccount.fetch(
+//       baseAccount.publicKey
+//     );
+//     console.log("Updated greeting:", account.greeting);
+//     assert.equal(account.greeting, "Updated greeting");
+//   });
+// });
+
 describe("hello_solana", () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
+	const provider = anchor.AnchorProvider.env();
+	anchor.setProvider(provider);
 
-  const program = anchor.workspace.HelloSolana as Program<HelloSolana>;
+	const program = anchor.workspace.HelloSolana as Program<HelloSolana>;
 
-  let baseAccount = anchor.web3.Keypair.generate();
+	let baseAccount = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    await program.methods
-      .initialize("Hello Solana!")
+	it ("<Test Init>", async () => {
+		await program.methods
+      .initialize()
       .accounts({
         baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey,
@@ -22,25 +63,10 @@ describe("hello_solana", () => {
       .signers([baseAccount])
       .rpc();
 
-    const account = await program.account.greetingAccount.fetch(
+    const account = await program.account.greetingUser.fetch(
       baseAccount.publicKey
     );
-    console.log("Stored greeting:", account.greeting);
-    assert.equal(account.greeting, "Hello Solana!");
-  });
-
-  it("Can update the message", async () => {
-    await program.methods
-      .update("Updated greeting")
-      .accounts({
-        baseAccount: baseAccount.publicKey,
-      })
-      .rpc();
-
-    const account = await program.account.greetingAccount.fetch(
-      baseAccount.publicKey
-    );
-    console.log("Updated greeting:", account.greeting);
-    assert.equal(account.greeting, "Updated greeting");
-  });
+    console.log("Owner:", account.owner, "amount:", account.amount, "self greets:", account.selfGreets);
+    // assert.equal(account.greeting, "Hello Solana!");
+	});
 });
