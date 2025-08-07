4f8426539d8ab4c639f004929b4dccc039f4f2e7
HelloX3030 <120511705+HelloX3030@users.noreply.github.com>
2025-08-07 14:50:11 +0000

    added checks for self greet and normal greet

--- tests/hello_solana.ts
+++ tests/hello_solana.ts
@@ -54,21 +54,32 @@ describe("hello_solana", () => {
 
 	let Account1: anchor.web3.Keypair;
 	let Account2: anchor.web3.Keypair;
+	let wallet1: anchor.web3.Keypair;
+	let wallet2: anchor.web3.Keypair;
 
 
 	before(async () => {
 		// Generate Keys
 		Account1 = anchor.web3.Keypair.generate();
 		Account2 = anchor.web3.Keypair.generate();
+		wallet1 = anchor.web3.Keypair.generate();
+		wallet2 = anchor.web3.Keypair.generate();
+
+		// Airdrop SOL to wallet1 and wallet2
+		const airdropSig1 = await provider.connection.requestAirdrop(wallet1.publicKey, 1e9);
+		await provider.connection.confirmTransaction(airdropSig1);
+
+		const airdropSig2 = await provider.connection.requestAirdrop(wallet2.publicKey, 1e9);
+		await provider.connection.confirmTransaction(airdropSig2);
 
 		// Account 1 setup
 		await program.methods
 		.initialize()
 		.accounts({
 			baseAccount: Account1.publicKey,
-			user: provider.wallet.publicKey,
+			user: wallet1.publicKey,
 		})
-		.signers([Account1])
+		.signers([wallet1, Account1])
 		.rpc();
 
 		// Account 2 setup
@@ -76,26 +87,31 @@ describe("hello_solana", () => {
 		.initialize()
 		.accounts({
 			baseAccount: Account2.publicKey,
-			user: provider.wallet.publicKey,
+			user: wallet2.publicKey,
 		})
-		.signers([Account2])
+		.signers([wallet2, Account2])
 		.rpc();
 	})
 
 	it ("<Test Init>", async () => {
 		let testAccount = anchor.web3.Keypair.generate();
+		
+		let testWallet = anchor.web3.Keypair.generate();
+		const airdropSignature = await provider.connection.requestAirdrop(testWallet.publicKey, 1_000_000_000); // 1 SOL
+		await provider.connection.confirmTransaction(airdropSignature);
+
 		await program.methods
 		.initialize()
 		.accounts({
 			baseAccount: testAccount.publicKey,
-			user: provider.wallet.publicKey,
+			user: testWallet.publicKey,
 		})
-		.signers([testAccount])
+		.signers([testWallet, testAccount])
 		.rpc();
 
 		const account = await program.account.greetingUser.fetch(testAccount.publicKey);
 		console.log("Owner:", account.owner, "amount:", account.amount, "self greets:", account.selfGreets);
-		assert(account.owner.equals(provider.wallet.publicKey))
+		assert(account.owner.equals(testWallet.publicKey));
 		assert(account.amount.eq(new BN(0)));
 		assert(account.selfGreets.eq(new BN(0)));
 	});
@@ -104,13 +120,26 @@ describe("hello_solana", () => {
 		await program.methods.greet()
 		.accounts({
 			baseAccount: Account1.publicKey,
-			user: provider.wallet.publicKey,
-		}).rpc();
+			user: wallet1.publicKey,
+		}).signers([wallet1]).rpc();
 
 		const account = await program.account.greetingUser.fetch(Account1.publicKey);
 		console.log("Owner:", account.owner, "amount:", account.amount, "self greets:", account.selfGreets);
-		assert(account.owner.equals(provider.wallet.publicKey))
+		assert(account.owner.equals(wallet1.publicKey));
 		assert(account.amount.eq(new BN(0)));
 		assert(account.selfGreets.eq(new BN(1)));
+	});
+
+	it ("<Test Other Greet>", async () => {
+		await program.methods.greet()
+		.accounts({
+			baseAccount: Account1.publicKey,
+			user: wallet2.publicKey,
+		}).signers([wallet2]).rpc();
+
+		const account = await program.account.greetingUser.fetch(Account1.publicKey);
+		console.log("Owner:", account.owner, "amount:", account.amount, "self greets:", account.selfGreets);
+		assert(account.owner.equals(wallet1.publicKey));
+		assert(account.amount.eq(new BN(1)));
 	});
 });
